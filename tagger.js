0492988ea5b6ac645e5971f51fdb98ba82c1efcc
Jakub Jankiewicz <jcubic@onet.pl>
2023-07-28 15:37:26 +0200

    version 0.6.0

--- tagger.js
+++ tagger.js
@@ -3,7 +3,7 @@
  * |_   _|___ ___ ___ ___ ___
  *   | | | .'| . | . | -_|  _|
  *   |_| |__,|_  |_  |___|_|
- *           |___|___|   version 0.5.0
+ *           |___|___|   version 0.6.0
  *
  * Tagger - Zero dependency, Vanilla JavaScript Tag Editor
  *
